ab722304ac9b0f3157b633e46bc9ae8852356825
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-01-27 21:54:07 -0600

    Added in functionality for grades api to take in a list of grade objects not just one

--- source/tektrakker/base/api.py
+++ source/tektrakker/base/api.py
@@ -1,5 +1,6 @@
 from base.models import Grade
-from rest_framework import viewsets, permissions
+from rest_framework import status, viewsets, permissions
+from rest_framework.response import Response
 from .serializers import GradeSerializer
 
 #Grade viewset
@@ -10,5 +11,9 @@ class GradeViewSet(viewsets.ModelViewSet):
     
     serializer_class = GradeSerializer
     
-    
-        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
