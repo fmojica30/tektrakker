7c18911de2f23045d9d840a9e184445193a5439d
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-01-30 08:16:49 -0600

    Succesful posting of the student data into the databse

--- source/tektrakker/frontend/src/components/NewStudentForm/NewStudentForm.js
+++ source/tektrakker/frontend/src/components/NewStudentForm/NewStudentForm.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const NewSudentForm = props => {
+  const [studentName, setStudentName] = useState("");
+  const [studentId, setStudentId] = useState("");
+
+  const submitStudentHandler = () => {
+    const studentData = {
+      name: studentName,
+      student_id: studentId
+    };
+
+    console.log(studentData);
+    axios
+      .post("/api/students/", studentData)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="card" style={{ marginTop: "120px" }}>
       <div className="card-body">
         <h1 className="text-center">New Student</h1>
-        <form>
+        <form onSubmit={submitStudentHandler}>
           <div className="form-group">
-            <label htmlFor="student-name">Student Name Name: </label>
-            <input type="text" className="form-control" id="student-name" />
+            <label htmlFor="student-name">Student Name: </label>
+            <input
+              type="text"
+              className="form-control"
+              id="student-name"
+              onChange={e => {
+                e.preventDefault;
+                setStudentName(e.target.value);
+                console.log(studentName);
+              }}
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="student-id">Student Name Name: </label>
-            <input type="text" className="form-control" id="student-id" />
+            <label htmlFor="student-id">Student id: </label>
+            <input
+              type="text"
+              className="form-control"
+              id="student-id"
+              onChange={e => {
+                e.preventDefault;
+                setStudentId(e.target.value);
+                console.log(studentId);
+              }}
+            />
           </div>
           <button type="onSubmit" className="btn btn-secondary">
             Submit
