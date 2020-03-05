aad9bd96496fd78d876265219a478f161d961eb2
Fernando Martinez Mojica <fernando.mojica@utexas.edu>
2020-03-05 15:37:02 -0600

    Past auth commits

--- backend/tek_backend/assignment/models.py
+++ backend/tek_backend/assignment/models.py
@@ -28,4 +28,4 @@ class PredictedGrade(models.Model):
     student = models.ForeignKey('groups.Student', on_delete=models.SET_NULL, null=True)
     prediction = models.DecimalField(max_digits=5, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+
